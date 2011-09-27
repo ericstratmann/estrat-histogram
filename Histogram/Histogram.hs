1f49ae7d70e9187d98fd51a4892be5895fd0bdf0
Eric Stratmann <estrat@cs.stanford.edu>
2011-09-26 23:54:11 -0700

    Added word formatting

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -1,4 +1,3 @@
-import Data.Char
 import Data.List
 import qualified Data.ByteString.Char8 as BS
 
@@ -6,33 +5,43 @@ main :: IO ()
 main = do
        contents <- BS.getContents
        let wordsList = BS.splitWith notWord contents
-       let sorted = sort wordsList
-       let count = getCount sorted
-       let histogram = wordCountToHistogram count
+       let wordsCount = getCount (sort wordsList)
+       let histogram = wordCountToHistogram wordsCount
        BS.putStrLn (BS.unlines histogram)
 
 notWord :: Char -> Bool
-notWord c = BS.elem c (BS.pack "?.,/;!")
+notWord c = BS.elem c (BS.pack "\n ?.,/;!")
 
+format :: BS.ByteString -> Int -> BS.ByteString
+format str maxLen 
+    | BS.length str > maxLen = BS.concat [BS.take (maxLen - BS.length dots) str, dots]
+    | otherwise = BS.concat [BS.replicate (maxLen - BS.length str) ' ', str]
+        where dots = BS.pack("..")
 
-wordCountToHistogram :: [(BS.ByteString, Integer)] -> [BS.ByteString]
+wordCountToHistogram :: [(BS.ByteString, Int)] -> [BS.ByteString]
 wordCountToHistogram count = histogram where
     sorted = sortBy sortPair count
-    histogram =  map toHistogramLine sorted
+    maxLen = min (BS.length $ fst $ maximumBy wordLen count) 15
+    histogram =  map toHistogramLine sorted where
+        toHistogramLine (word, c) =
+            BS.concat [(format word maxLen), BS.pack(": "), BS.replicate c 'x']
         
-toHistogramLine :: (BS.ByteString, Integer) -> BS.ByteString
-toHistogramLine (word, c) =  word -- ++ ": " ++ take (fromIntegral c) (repeat 'x')
 
-sortPair :: (BS.ByteString, Integer) -> (BS.ByteString, Integer) -> Ordering
+wordLen :: (BS.ByteString, Int) -> (BS.ByteString, Int) -> Ordering
+wordLen (a,_) (b,_) | diff < 0 = LT
+                   | diff > 0 = GT
+                   | diff == 0 = EQ
+                   where diff = BS.length a - BS.length b
+wordLen _ _ = EQ
+
+sortPair :: (BS.ByteString, Int) -> (BS.ByteString, Int) -> Ordering
 sortPair (_,a) (_,b) | a < b = LT
                      | a > b = GT
                      | otherwise = EQ
 
-getCount :: [BS.ByteString] -> [(BS.ByteString, Integer)]
+getCount :: [BS.ByteString] -> [(BS.ByteString, Int)]
 getCount = getCount' [] 1 where 
     getCount' acc c [w] = (w,c) : acc
     getCount' acc c (w:w':ws) | w == w' = getCount' acc (c+1) (w':ws)
                               | otherwise = getCount' ((w,c):acc) 1 (w':ws)
     getCount' _ _ _ = []
-
-isWordSep c = isPunctuation c || isSpace c
