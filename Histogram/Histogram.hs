46ec3581f15bbc646020b102e8afd1805939950e
Eric Stratmann <estrat@cs.stanford.edu>
2011-10-02 21:31:54 -0700

    Readability cleanups

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -2,17 +2,14 @@ import System.Environment
 import Data.List
 import Data.Char
 import Data.Map
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.Lazy.Char8 as LBS
 
-nonWordChars :: [Char]
-nonWordChars = "\n ?.,/:;!@#$1234567890()-_=+[]{}|\\\t"
+-- This program outputs an ASCII histogram based on word frequncy
 
 maxWidth :: Int
 maxWidth = 80
 
-maxcompareWordLength :: Int
-maxcompareWordLength = 15
+maxWordLength :: Int
+maxWordLength = 15
 
 histogramChar :: Char
 histogramChar = '#'
@@ -20,74 +17,93 @@ histogramChar = '#'
 main :: IO ()
 main = do
     input <- readInput
-    let lower = {-# SCC "toLower" #-} LBS.map toLower input
-    let wordsList = {-# SCC "split" #-} LBS.splitWith notWord lower
-    let map = {-# SCC "buildMap" #-} buildMap wordsList
-    let wordsCount = {-# SCC "MapToList" #-} toList map
-    let histogram = {-# SCC "toHistogram" #-} wordCountToHistogram wordsCount
-    BS.putStrLn (BS.unlines histogram)
+    let lower = fmap toLower input
+    let wordsList = getWords lower
+    let mapo = buildMap wordsList
+    let wordsCount = toList mapo
+    let histogram = wordCountToHistogram wordsCount
+    putStrLn (unlines histogram)
 
-readInput :: IO (LBS.ByteString)
+-- Returns all files passed in as input or any data from STDIN.
+readInput :: IO String
 readInput = do
     args <- getArgs
-    if length args == 0
+    if Data.List.null args
         then 
-            LBS.getContents
+            getContents
         else do
-            fileData <- mapM LBS.readFile args
-            return $ LBS.concat fileData
+            fileData <- mapM readFile args
+            return $ concat fileData
 
-buildMap :: [LBS.ByteString] -> Map BS.ByteString Int
+-- Given a list of words, builds a map containing a frequncy count of each word.
+buildMap :: [String] -> Map String Int
 buildMap = foldl' insertMap empty
 
-insertMap :: Map BS.ByteString Int -> LBS.ByteString -> Map BS.ByteString Int
-insertMap map val =
-     let strictVal = safeGet $ LBS.toChunks val in
-        alter updateVal strictVal map
+-- Updates the word count in the map for the given word.
+insertMap :: Map String Int -> String -> Map String Int
+insertMap map val = alter updateVal val map
 
+-- Updates a Maybe to increment the word count by 1.
 updateVal :: Maybe Int -> Maybe Int
 updateVal (Just i) = Just (i+1)
 updateVal Nothing = Just 1
 
-safeGet :: [BS.ByteString] -> BS.ByteString
-safeGet [x] = x
-safeGet _ = BS.empty
+-- Given a list of (word, count) pairs, returns a list of strings that output
+-- a histogram when printed.
+wordCountToHistogram :: [(String, Int)] -> [String]
+wordCountToHistogram wordCount = convertToASCII sortedWordLen maxWordLen where
+    sortedWordCount = sortBy compareFreqLen wordCount
+    maxWordLen = getMaxWordLen sortedWordCount
+    maxWordFreq = getMaxWordFreq sortedWordCount
+    sortedWordLen = Data.List.filter notEmptyLine $ fmap histoLen sortedWordCount
+    histoLen (word, len) = (word ,div (maxHistogramLen * len) maxWordFreq)
+    maxHistogramLen = maxWidth - maxWordLen - 1
 
-notWord :: Char -> Bool
-notWord c = BS.elem c nonWordCharsBS
+-- Returns whether the histogram line would not be empty when printed
+notEmptyLine :: (String, Int) -> Bool
+notEmptyLine (word, len) = len > 0
 
-nonWordCharsBS :: BS.ByteString
-nonWordCharsBS = BS.pack nonWordChars
+-- Returns the length of the longest word
+getMaxWordLen :: [(String, Int)] -> Int
+getMaxWordLen sorted  = min (length . fst $ maximumBy compareWordLen sorted) maxWordLength
 
-format :: BS.ByteString -> Int -> BS.ByteString
+-- Returns the largest word count in a list that is sorted max first
+getMaxWordFreq :: [(String, Int)] -> Int
+getMaxWordFreq = snd . head 
+
+-- Generates ASCII text for each (word, len) pair
+convertToASCII :: [(String, Int)] -> Int -> [String]
+convertToASCII wordLens maxWordLen = fmap toHistogramLine wordLens where
+    toHistogramLine (word, len) = concat [format word maxWordLen, " " , replicate len histogramChar]
+ 
+-- Formats a word so that it will print nicely
+format :: String -> Int -> String
 format str maxLen 
-    | BS.length str > maxLen = BS.concat [BS.take (maxLen - BS.length dots) str, dots]
-    | otherwise = BS.concat [BS.replicate (maxLen - BS.length str) ' ', str]
-        where dots = BS.pack ".." 
-
-
-wordCountToHistogram :: [(BS.ByteString, Int)] -> [BS.ByteString]
-wordCountToHistogram count = histogram where
-    sorted = sortBy compareFreqLen (dropEmpty count)
-    maxcompareWordLen = min (BS.length . fst $ maximumBy compareWordLen count) maxcompareWordLength
-    maxHistogramLen = snd $ head sorted
-    histogram =  fmap toHistogramLine sorted
-    toHistogramLine (word, c) = BS.concat [format word maxcompareWordLen, BS.pack ": " , BS.replicate histoLen histogramChar] where
-        histoLen = max (div (maxHistoLen * c) maxHistogramLen) 1
-    maxHistoLen = maxWidth - maxcompareWordLen - 2
-
-dropEmpty :: [(BS.ByteString, Int)] -> [(BS.ByteString, Int)]
-dropEmpty (x:xs) | BS.length (fst x) == 0 = xs
-                 | otherwise = x : dropEmpty xs
-dropEmpty x = x
+    | length str > maxLen = take (maxLen - length dots) str ++ dots
+    | otherwise = str ++ replicate (maxLen - length str) ' '
+        where dots = ".." 
         
-compareWordLen :: (BS.ByteString, Int) -> (BS.ByteString, Int) -> Ordering
+-- Sorts (word, count) pairs by shortest word first
+compareWordLen :: (String, Int) -> (String, Int) -> Ordering
 compareWordLen (a,_) (b,_) | diff < 0 = LT
                            | diff > 0 = GT
                            | otherwise = EQ
-                               where diff = BS.length a - BS.length b
+                               where diff = length a - length b
 
-compareFreqLen :: (BS.ByteString, Int) -> (BS.ByteString, Int) -> Ordering
+-- Sorts (word, count) pairs by largest frequency first
+compareFreqLen :: (String, Int) -> (String, Int) -> Ordering
 compareFreqLen (_,a) (_,b) | a > b = LT
                            | a < b = GT
                            | otherwise = EQ
+
+-- Returns a list of all words in the string. Adapted from `words' in the
+-- Haskell Standard Prelude.
+getWords s
+  | findSpace == [] = []
+  | otherwise = w : getWords s'' where
+      (w, s'') = break isDelim findSpace
+      findSpace = dropWhile isDelim s
+
+-- Returns whether is a delimiter character that separates words
+isDelim :: Char -> Bool
+isDelim s = isPunctuation s || isSpace s
